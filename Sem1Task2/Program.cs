1bbc43b9010bfebade544749b57af803d959d3bf
avevitaliy1987 <120784550+avevitaliy1987@users.noreply.github.com>
2023-01-15 13:01:32 +0500

    add solution for task 0-8

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿//Задача №2
+//Напишите программу, которая на вход принимает
+//два числа и выдает, какое число больше, а какое меньше
+
+//выводим запрос на ввод числа
+Console.Write("Введите число А  ");
+//Считываем данные с консоли 
+string? imputNumLine1 = Console.ReadLine();
+//выводим запрос на ввод числа
+Console.Write("Введите число Б  ");
+//Считываем данные с консоли 
+string? imputNumLine2 = Console.ReadLine();
+
+
+// Проверяем, чтобы данные не были пустыми
+if(imputNumLine1!=null && imputNumLine2!=null)
+{
+// Парсим введенное число
+    int imputNumberA = int.Parse(imputNumLine1);
+  
+    int imputNumberБ = int.Parse(imputNumLine2);
+    
+
+    //задаем условие 
+    if(imputNumberA < imputNumberБ)
+    {
+     Console.WriteLine("Число Б больше числа А");
+    }
+    else
+    {
+     Console.WriteLine("Число А больше числа Б");
+    }
+    
+}
+
---
+++ Sem1Task4/Program.cs
@@ -0,0 +1,61 @@
+﻿//Задача №4
+//Напишите программу, которая на вход принимает
+//три числа и выдаетмаксимальное из этих чисел
+
+
+//выводим запрос на ввод числа
+Console.Write("Введите число A  ");
+//Считываем данные с консоли 
+string? imputNumLine1 = Console.ReadLine();
+
+//выводим запрос на ввод числа
+Console.Write("Введите число B  ");
+//Считываем данные с консоли 
+string? imputNumLine2 = Console.ReadLine();
+
+//выводим запрос на ввод числа
+Console.Write("Введите число C  ");
+//Считываем данные с консоли 
+string? imputNumLine3 = Console.ReadLine();
+
+// Проверяем, чтобы данные не были пустыми
+if(imputNumLine1!=null && imputNumLine2!=null && imputNumLine3!=null)
+{
+// Парсим введенное число
+    int imputNumberA = int.Parse(imputNumLine1);
+  
+    int imputNumberB = int.Parse(imputNumLine2);
+   
+    int imputNumberC = int.Parse(imputNumLine3);
+    
+    //задаем условие 
+    if(imputNumberA < imputNumberB)
+    {
+        if(imputNumberB > imputNumberC)
+        {
+            Console.WriteLine("Максимальное число = - число B = ");
+             Console.WriteLine(imputNumLine2);
+            }
+        
+        else
+        {
+           Console.WriteLine("Максимальное число - число C = "); 
+           Console.WriteLine(imputNumLine3);
+            
+        }
+
+    }
+    else
+        if(imputNumberA > imputNumberC)
+        {
+            Console.WriteLine("Максимальное число - число A = ");
+            Console.WriteLine(imputNumLine1);
+            }
+        
+        else
+        {
+            Console.WriteLine("Максимальное число - число C = ");
+            Console.WriteLine (imputNumLine3);
+           
+        }
+}
