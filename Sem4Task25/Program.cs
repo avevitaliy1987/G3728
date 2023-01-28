1d65c76653e29ca91eb56eae1e8a6edaf375590a
avevitaliy1987 <120784550+avevitaliy1987@users.noreply.github.com>
2023-01-28 11:43:57 +0500

    add solution task 25, 27,29

---
+++ Sem4Task25/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B.
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//Выводим результат пользователю
+void PrintData(string msg1, int msg2)
+{
+    Console.WriteLine(msg1);
+    Console.WriteLine(msg2);
+}
+
+int Pow(int a, int b)
+{
+    int res = 1;
+    while(b>0)
+    {
+        b=b-1;
+        res=res*a;
+    }
+// завершение выполнение метода и возврат результата функции вызвавшему методу
+return res;
+}
+
+int a = ReadData("Введите первое число А: ");
+int b = ReadData("Введите второе число Б: ");
+
+int result = Pow(a, b);
+
+PrintData("Результат возведения числа Ав степень числа Б равен:", result);
---
+++ Sem4Task27/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход 
+//число и выдаёт сумму цифр в числе.
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//Выводим результат пользователю
+void PrintData(int msg)
+{
+    Console.WriteLine(msg);
+}
+
+// задаем метод
+int SumSym(int num)
+{
+    int res = 0;
+//описание функции метода
+    while(num>0)
+    {
+        res=res+num%10;
+        num=num/10;
+    }
+// завершение выполнение метода и возврат результата функции вызвавшему методу
+return res;
+}
+
+int num = ReadData("Введите число: ");
+
+int sum = SumSym(num);
+
+PrintData(sum);
