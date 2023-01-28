40bfe381570b563767fafb4933ee686df87147d6
avevitaliy1987 <120784550+avevitaliy1987@users.noreply.github.com>
2023-01-28 11:06:28 +0500

    add solutions task 31, 32, 33, 35, 37

---
+++ Sem5Task32/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача №32
+//Напишите программу замена элементов массива: положительные 
+//элементы замените на соответствующие отрицательные, и наоборот.
+
+
+
+// int ReadData(string msg)
+// {
+//     Console.WriteLine(msg);
+//     return int.Parse(Console.ReadLine() ?? "0");
+// }
+
+
+//метод генерации массива
+int[] Gen1DArr(int num, int begin, int end)
+
+{
+    // метод для генерации массива
+    Random rnd = new Random();
+    int[] arr = new int[num];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(begin, end);
+    }
+    return arr;
+}
+
+void Print1DArr(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length-1; i++)
+    {
+        Console.Write(arr[i]+", ");
+    }
+    Console.WriteLine(arr[arr.Length-1]+"]");
+}
+
+void InvArr(int[] arr)
+{
+    for(int i = 0; i < arr.Length;i++)
+    {
+        arr[i]*=-1;
+        }
+}
+int[] testArr = Gen1DArr(12,-9,9);
+Print1DArr(testArr);
+InvArr(testArr);
+Print1DArr(testArr);
